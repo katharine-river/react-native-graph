5ec8035783b7cd42b987e7166cd8056a29d7e7c8
katharine-river <132921466+katharine-river@users.noreply.github.com>
2024-07-12 10:14:17 -0300

    fix: fix path interpolation animation

--- src/AnimatedLineGraph.tsx
+++ src/AnimatedLineGraph.tsx
@@ -265,6 +265,7 @@ export function AnimatedLineGraph({
 
     setCommandsChanged(commandsChanged + 1)
 
+    interpolateProgress.value = 0
     interpolateProgress.value = withSpring(1, {
       mass: 1,
       stiffness: 500,
